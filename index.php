0ecece7b318489bb3c74420a1271edb3d9c3eeeb
a_athaullah <ahmad.athaullah@gmail.com>
2019-03-06 11:49:57 +0700

    initial commmit

---
+++ index.php
@@ -0,0 +1,47 @@
+<?php
+header('Content-type: application/json');
+
+$db_host = "ec2-174-129-236-21.compute-1.amazonaws.com";
+$db_name = "d39pgl7bkn08d4";
+$db_user = "efzoujkkvdnkkp";
+$db_pass = "b8149b10a3c2a78eff917d1d0162e3fe800bd1283916b57f4e82265f6fe0b3f2";
+$db_port = "5432";
+
+$db_conn_string = "host=".$db_host." port=".$db_port." dbname=".$db_name." user=".$db_user." password=".$db_pass;
+
+$db = pg_connect($db_conn_string);
+
+if (!$db) {
+    echo "DB error occurred.\n";
+    exit;
+}
+
+$query = "SELECT * FROM product";
+
+if (isset($_REQUEST['keyword'])){
+    $query = $query." WHERE title LIKE '%".$_REQUEST['keyword']."%'";
+}
+
+
+$query .= " LIMIT 5";
+
+// echo $query;
+
+$result = pg_query($db, $query);
+if (!$result) {
+    echo "An error occurred.\n";
+    exit;
+}
+
+$arr = pg_fetch_all($result);
+
+pg_close($db);  
+// print_r($arr);
+
+$productJSON = json_encode($arr);
+
+echo $productJSON;
+
+
+
+?>
